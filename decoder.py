9b83beac7e4d0d3fbcefc6816ef46516826b909b
Scott Mansell <phiren@gmail.com>
2020-12-11 19:07:53 +1300

    Make dummy decoder smarter
    Otherwise the synthisis tools can get a little too smart and
    optimise things away down stream based on the fact
    all instrctions are identical

--- decoder.py
+++ decoder.py
@@ -14,21 +14,44 @@ class Decoder(Elaboratable):
         self.immediate = Signal(16)
         self.valid = Signal(3) # one for each reg
 
+        self.dummy_icache = Memory(width=5*3, depth=256, init=[
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00001,
+            0b00001_00010_00011,
+            0b00001_00010_00011,
+            0b00011_00010_00101,
+            0b00011_00000_01101,
+            0b00011_00000_01101,
+            0b00011_10000_01101,
+            0b01001_01000_01101,
+            0b00101_00100_01101,
+            0b10001_00110_01101,
+        ] * 15)
+
     def elaborate(self, platform):
         m = Module()
 
         # Takes in 32bit opcode
 
+        counter = Signal(8)
+
         m.d.sync += [
-            # Just continually output a dummy instruction
-            # This is something like add r1, r1, r2
+            # Just continually output dummy instructions from icache
             self.executionUnits.eq(Const(7)), # Can execute on execution units 1, 2 or 3
             self.opcode.eq(Const(13)), # random opcode. Might mean add
-            self.regA.eq(Const(1)),
-            self.regB.eq(Const(2)),
-            self.regOut.eq(Const(1)),
+            self.regA.eq(self.dummy_icache[counter][10:14]),
+            self.regB.eq(self.dummy_icache[counter][5:9]),
+            self.regOut.eq(self.dummy_icache[counter][0:4]),
             self.immediate.eq(Const(0)), # unused
-            self.valid.eq(Const(7))
+            self.valid.eq(Const(7)),
+
+            counter.eq(counter + 1)
         ]
 
         # Outputs:
